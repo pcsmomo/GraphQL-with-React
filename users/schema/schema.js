13a1f99b757d199554737014751033956b714166
pcsmomo <dongwoo_333@hanmail.net>
2021-06-16 10:33:45 +1000

    18. Nested Queries

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -3,12 +3,24 @@ const _ = require("lodash");
 const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType
+    }
   }
 });
 
