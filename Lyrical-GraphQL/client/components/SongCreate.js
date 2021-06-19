b447650ed6e33041fc85122cf929907eeb13778a
pcsmomo <dongwoo_333@hanmail.net>
2021-06-19 22:34:53 +1000

    45. Creating a Song

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SongCreate = () => {
+  const [title, setTitle] = useState("");
+
   return (
     <div>
       <h3>Create a New Song</h3>
-      <form></form>
+      <form>
+        <label>Song Title :</label>
+        <input
+          onChange={(event) => setTitle(event.target.value)}
+          value={title}
+        />
+      </form>
     </div>
   );
 };
