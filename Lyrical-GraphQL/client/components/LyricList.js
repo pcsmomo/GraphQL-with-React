c98e1c5dac7a8ed1c5ab0fb6b14f02bb890e1872
pcsmomo <dongwoo_333@hanmail.net>
2021-06-22 16:49:58 +1000

    69. Thumbs Up Icon

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const LyricList = ({ lyrics }) => {
+  const onLike = (id) => {
+    console.log(id);
+  };
+
   const renderLyrics = () => {
     return lyrics.map(({ id, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <i className="material-icons" onClick={() => onLike(id)}>
+            thumb_up
+          </i>
         </li>
       );
     });
