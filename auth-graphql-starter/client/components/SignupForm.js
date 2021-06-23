e90a31d4cefa9104de595e7f3c220213327abdf8
pcsmomo <dongwoo_333@hanmail.net>
2021-06-24 07:08:37 +1000

    106. Handling Errors Around Signup

--- auth-graphql-starter/client/components/SignupForm.js
+++ auth-graphql-starter/client/components/SignupForm.js
@@ -4,20 +4,29 @@ import { useMutation } from "@apollo/client";
 import AuthForm from "./AuthForm";
 
 import SIGNUP from "../mutations/Signup";
+import CURRENT_USER from "../queries/CurrentUser";
 
 const SingupForm = () => {
+  const [errors, setErrors] = useState([]);
   const [signup] = useMutation(SIGNUP);
 
   const submit = ({ email, password }) => {
     signup({
-      variables: { email, password }
+      variables: { email, password },
+      refetchQueries: [{ query: CURRENT_USER }]
+    }).catch((res) => {
+      setErrors(
+        res.graphQLErrors.map(({ message }) =>
+          message.substring(message.indexOf('"') + 1, message.length - 1)
+        )
+      );
     });
   };
 
   return (
     <div>
       <h3>Sign Up</h3>
-      <AuthForm errors={[]} submit={submit} />
+      <AuthForm errors={errors} submit={submit} />
     </div>
   );
 };
