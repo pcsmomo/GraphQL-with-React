9e215d8d504ce472672d88c4070a00474ccfcd8b
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 14:36:15 +1000

    90. Root Routes with React Router

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from "history";
+
+import App from "./components/App";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -14,10 +18,14 @@ const client = new ApolloClient({
   })
 });
 
+const history = createBrowserHistory();
+
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={history}>
+        <Route path="/" component={App}></Route>
+      </Router>
     </ApolloProvider>
   );
 };
