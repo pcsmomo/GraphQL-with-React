e8eac72b3aa5a793c7a5841e0d841e4e9a9d582a
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 14:18:30 +1000

    89. Client Side Setup

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache({
+    typePolicies: {
+      user: {
+        keyFields: "id"
+        // or keyFields: ["id"]
+      }
+    }
+  })
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
