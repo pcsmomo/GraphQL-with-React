48b39e28e808d1dd31808b0afe9df8cfb45c9218
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 17:39:01 +1000

    95. Handling the Logout Mutation

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
-import query from "../queries/CurrentUser";
+import CURRENT_USER from "../queries/CurrentUser";
+import LOGOUT from "../mutations/Logout";
 
 const Header = () => {
-  const { data, loading } = useQuery(query);
+  const { data, loading } = useQuery(CURRENT_USER);
+  const [logout] = useMutation(LOGOUT);
+
+  const onLogoutClick = () => {
+    logout();
+  };
 
   const renderButtons = () => {
     if (loading) return <div />;
 
     if (data.user) {
-      return <div>Logout</div>;
+      return (
+        <li>
+          <a onClick={onLogoutClick}>Logout</a>
+        </li>
+      );
     } else {
       return (
         <div>
