2da2002770b08070d5c326f8a3a64f800c39cfb1
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 21:54:14 +1000

    98. The Auth Form

--- auth-graphql-starter/client/components/LoginForm.js
+++ auth-graphql-starter/client/components/LoginForm.js
@@ -1,7 +1,13 @@
 import React from "react";
+import AuthForm from "./AuthForm";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div className="container">
+      <h3>Login</h3>
+      <AuthForm />
+    </div>
+  );
 };
 
 export default LoginForm;
