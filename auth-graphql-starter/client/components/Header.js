d8d016d3cb28d605aebb03aa4e00407c25658675
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 14:50:27 +1000

    91. Figuring Out the Current User

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import query from "../queries/CurrentUser";
 
 const Header = () => {
+  // const { data, loading } = useQuery(query);
+  const queryRes = useQuery(query);
+  console.log(queryRes);
+  const { data, loading } = queryRes;
+
+  if (loading) return <div>Loading...</div>;
+
   return <div>Header</div>;
 };
 
