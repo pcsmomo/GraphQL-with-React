d3514805dde203411e79205860748252cfefa69f
pcsmomo <dongwoo_333@hanmail.net>
2021-06-23 21:59:55 +1000

    99. Importing the Login Mutation

--- auth-graphql-starter/client/components/LoginForm.js
+++ auth-graphql-starter/client/components/LoginForm.js
@@ -1,7 +1,11 @@
 import React from "react";
 import AuthForm from "./AuthForm";
+import { useMutation } from "@apollo/client";
+import LOGIN from "../mutations/Login";
 
 const LoginForm = () => {
+  const [login] = useMutation(LOGIN);
+
   return (
     <div className="container">
       <h3>Login</h3>
